1d5ff60f28177321247017d7f68a6399247f7f36
asmabensaad <bensaad.asma10@gmail.com>
2023-06-24 10:34:53 +0100

    second commit     public/assets/     src/Dashboard/ 	src/auth/ 	src/components/ 	src/data/ 	src/scenes/ 	src/theme.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Dashboard from "./Dashboard/Dashboard.jsx";
+
+import First from "./auth/First.jsx";
+import Index from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
+import Secrets from "./scenes/secrets";
+import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Routes>
+      <Route path="" element={<Dashboard />} >
+        <Route path="/" element={<Index />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/secrets" element={<Secrets />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/geography" element={<Geography />} />
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Route>
+      <Route path="auth" element={<First />} />
+    </Routes>
+
   );
 }
 
